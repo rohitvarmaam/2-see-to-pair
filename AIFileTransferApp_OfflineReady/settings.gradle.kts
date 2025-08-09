50857f21391f2661469b5df415f212e048d37f0c
rohitvarmaam <rohitvarma.am@gmail.com>
2025-08-09 12:24:05 +0530

    Update settings.gradle.kts

--- AIFileTransferApp_OfflineReady/settings.gradle.kts
+++ AIFileTransferApp_OfflineReady/settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "AIFileTransferApp"
+pluginManagement {
+    repositories {
+        google()            // <-- needed for com.android.application
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()            // <-- Android libraries
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AIFileTransferApp_OfflineReady"
 include(":app")
